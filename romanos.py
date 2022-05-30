7df6bdbfae9a8846a412ae75ae26ae600aac0036
whasabi <jmartincarratala@outlook.es>
2022-05-30 23:51:01 +0200

    Descomponer el número

--- romanos.py
+++ romanos.py
@@ -7,13 +7,28 @@ def convertir_en_romano(numero):
             - no es mayor que 3999
     Resultado es una cadena que contiene (I, v, X, L, C, D, M)
 
-    ideas para comprobar un entero
-        -isdigit()
     """
     if not isinstance(numero, int):
        return"no has introducido un número"
     if numero < 0 or numero > 3999:
         return "El número introducido no es válido (debe ser un número positivo y menos de 4000)"
+
+    # continuamos la conversión
+    simbolos = {
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
+    }
     return "OK"
 
+    # Descomponer en unidades, decenas, centenas y unidades de millar
+    # Opcion 1: división entera + modulo en cascada
+    # Opción 2: convertir en cadena y en función de la longitud y la posición obtener u,d,c y um 
+
 print(convertir_en_romano(-3))
+print(convertir_en_romano(33333))
+print(convertir_en_romano(253))
